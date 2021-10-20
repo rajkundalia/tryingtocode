06e6d16c88f39731f7677e7104e3fd228b34f250
Raj Kundalia <rajakundalia@gmail.com>
2021-10-20 20:40:04 +0530

    Create GetKeyPadCombination.java

---
+++ 2021/GetKeyPadCombination.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetKeyPadCombination {
+    public static void main(String[] args) {
+        String str = "123";
+        List<String> list = getKPC(str);
+        System.out.println(list);
+    }
+
+    static String[] codes = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
+
+    private static List<String> getKPC(String str) {
+        if(str.length() == 0) {
+            List<String> mres = new ArrayList<>();
+            mres.add("");
+            return mres;
+        }
+        char ch = str.charAt(0);
+        String restOfString = str.substring(1);
+
+        List<String> rres = getKPC(restOfString);
+        List<String> mres = new ArrayList<>();
+
+        String codeForCh = codes[ch - '0'];
+        for (int i = 0; i < codeForCh.length(); i++) {
+            char c = codeForCh.charAt(i);
+            for (String st: rres) {
+                mres.add(c + st);
+            }
+        }
+        return mres;
+    }
+}
