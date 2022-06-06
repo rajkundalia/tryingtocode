064d89b861d7a63a8e062e97dca3478506f9ccbd
Raj Kundalia <rajakundalia@gmail.com>
2022-06-06 23:43:25 +0530

    Maximum Absolute Difference
    https://www.interviewbit.com/problems/maximum-absolute-difference/

---
+++ interviewbit/arrays/MaximumAbsoluteDifference.java
@@ -0,0 +1,41 @@
+package interviewbit.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+You are given an array of N integers, A1, A2 ,…, AN. Return maximum value of f(i, j) for all 1 ≤ i, j ≤ N.
+
+f(i, j) is defined as |A[i] - A[j]| + |i - j|, where |x| denotes absolute value of x.
+
+For example,
+
+A=[1, 3, -1]
+
+f(1, 1) = f(2, 2) = f(3, 3) = 0
+f(1, 2) = f(2, 1) = |1 - 3| + |1 - 2| = 3
+f(1, 3) = f(3, 1) = |1 - (-1)| + |1 - 3| = 4
+f(2, 3) = f(3, 2) = |3 - (-1)| + |2 - 3| = 5
+
+So, we return 5.
+ */
+public class MaximumAbsoluteDifference {
+    public static void main(String[] args) {
+        System.out.println(solveMaximumAbsoluteDifference(new ArrayList<>(List.of(1, 3, -1))));
+    }
+
+    public static int solveMaximumAbsoluteDifference(ArrayList<Integer> A) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for (int i = 0; i < A.size(); i++) {
+            max1 = Math.max(max1, A.get(i) + i);
+            min1 = Math.min(min1, A.get(i) + i);
+            max2 = Math.max(max2, A.get(i) - i);
+            min2 = Math.min(min2, A.get(i) - i);
+        }
+        return Math.max(max1 - min1, max2 - min2);
+    }
+}
