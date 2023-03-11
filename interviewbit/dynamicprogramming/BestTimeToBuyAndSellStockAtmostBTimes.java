e7b8ffa29530bcc31dc030048e2c18eaaba96e69
Raj Kundalia <rajakundalia@gmail.com>
2023-03-11 13:46:38 +0530

    Update BestTimeToBuyAndSellStockAtmostBTimes.java

--- interviewbit/dynamicprogramming/BestTimeToBuyAndSellStockAtmostBTimes.java
+++ interviewbit/dynamicprogramming/BestTimeToBuyAndSellStockAtmostBTimes.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /*
 Given an array of integers A of size N in which ith element is the price of the stock on day i.
-You can complete atmost B transactions.
+You can complete atmost B transactions i.e. B buys and B sells
 Find the maximum profit you can achieve.
 NOTE: You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
@@ -46,6 +46,9 @@ Explanation 2:
 public class BestTimeToBuyAndSellStockAtmostBTimes {
     public static void main(String[] args) {
         System.out.println(new BestTimeToBuyAndSellStockAtmostBTimes().solve(new int[]{2, 4, 8, 6, 7}, 2));
+
+        System.out.println(new BestTimeToBuyAndSellStockAtmostBTimes().maxProfit(new int[]{3, 2, 6, 5, 0, 3}, 2));
+        System.out.println(new BestTimeToBuyAndSellStockAtmostBTimes().maxProfitOptimized(new int[]{3, 2, 6, 5, 0, 3}, 2));
     }
 
     /*
@@ -59,6 +62,8 @@ public class BestTimeToBuyAndSellStockAtmostBTimes {
 
         dp table
      */
+    // This just solves the interviewbit problem, issue but is giving wrong output
+    // For input 3, 2, 6, 5, 0, 3 and k = 2, answer should be 7 but it is giving 11 and it is getting accepted
     public int solve(int[] a, int b) {
         int n = a.length;
         if (b >= n / 2) {
@@ -77,6 +82,54 @@ public class BestTimeToBuyAndSellStockAtmostBTimes {
                 md = Math.min(md, a[j - 1] - dp[i - 1][j]);
             }
         }
-        return dp[b][n];
+        return dp[b][n - 1];
+    }
+
+    // https://youtu.be/3YILP-PdEJA - explains the correct way
+    // 1st iteration
+    public int maxProfit(int[] arr, int k) {
+        int n = arr.length;
+        int[][] dp = new int[k + 1][n];
+
+        // rows are transactions, so outer loop is t for transaction numbers
+        for (int t = 1; t <= k; t++) {
+            // columns are d for days
+            for (int d = 1; d < n; d++) {
+                int max = dp[t][d - 1];
+                // pd = past day
+                for (int pd = 0; pd < d; pd++) {
+                    int profitTillTMinus1Transaction = dp[t - 1][pd];
+                    int profitOfTthTransaction = arr[d] - arr[pd];
+                    if (profitTillTMinus1Transaction + profitOfTthTransaction > max) {
+                        max = profitOfTthTransaction + profitTillTMinus1Transaction;
+                    }
+                }
+                dp[t][d] = max;
+            }
+        }
+        return dp[k][n - 1];
+    }
+
+    // 2nd iteration - optimization
+    public int maxProfitOptimized(int[] arr, int k) {
+        int n = arr.length;
+        int[][] dp = new int[k + 1][n];
+
+        // rows are transactions, so outer loop is t for transaction numbers
+        for (int t = 1; t <= k; t++) {
+            int max = Integer.MIN_VALUE;
+            // columns are d for days
+            for (int d = 1; d < n; d++) {
+                if (dp[t - 1][d - 1] - arr[d - 1] > max) {
+                    max = dp[t - 1][d - 1] - arr[d - 1];
+                }
+                if (max + arr[d] > dp[t][d - 1]) {
+                    dp[t][d] = max + arr[d];
+                } else {
+                    dp[t][d] = dp[t][d - 1];
+                }
+            }
+        }
+        return dp[k][n - 1];
     }
 }
