cbb4fb8e65ecbc6fc8748a2854c9c1b0f029139f
Raj Kundalia <rajakundalia@gmail.com>
2023-07-01 16:38:42 +0530

    Update TrappingRainWater.java

--- leetcode/top-interview-150/TrappingRainWater.java
+++ leetcode/top-interview-150/TrappingRainWater.java
@@ -48,4 +48,49 @@ public class TrappingRainWater {
 
         return ans;
     }
+
+    /*https://leetcode.com/problems/trapping-rain-water/solutions/3387829/cjavapythonjavascript-brute-force-optimized-code-easy-to-understand-for-begineers/ */
+
+    /*
+    Initialize left, right pointers to the first and last bars of the height array, respectively.
+    Initialize variables left_max and right_max to zero.
+    While the left pointer is less than or equal to the right pointer,
+    compare the heights of the bars pointed to by the left and right pointers.
+
+    If the height of the left bar is less than or equal to the height of the
+    right bar, check if the height of the left bar is greater than the left_max variable.
+
+    If it is, update left_max, otherwise, add left_max - height[left] to the "water" variable.
+    Move the left pointer to the next bar.
+
+    If the height of the right bar is less than the height of the left bar,
+    check if the height of the right bar is greater than the right_max variable.
+
+    If it is, update right_max, otherwise, add right_max - height[right] to the "water" variable.
+    Move the right pointer to the previous bar.
+
+    Return the value of the "water" variable.
+     */
+    public int trap1(int[] height) {
+        int n = height.length;
+        int left = 0, right = n - 1, left_max = 0, right_max = 0, water = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] > left_max) {
+                    left_max = height[left];
+                } else {
+                    water += left_max - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] > right_max) {
+                    right_max = height[right];
+                } else {
+                    water += right_max - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
 }
