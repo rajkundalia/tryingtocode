641c37bcb0f0eb0ac44727e6a00bf9c4b80bb01d
Raj Kundalia <rajakundalia@gmail.com>
2023-07-28 22:38:54 +0530

    Add files via upload

---
+++ leetcode/top-interview-150/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,133 @@
+package leetcode.topinterview150;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/*
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path
+from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+ */
+public class MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node20 = new TreeNode(20);
+        TreeNode node15 = new TreeNode(15);
+        TreeNode node7 = new TreeNode(7);
+
+        root.left = node9;
+        root.right = node20;
+
+        node20.left = node15;
+        node20.right = node7;
+
+        System.out.println(new MaximumDepthOfBinaryTree().maxDepth(root));
+        System.out.println(new MaximumDepthOfBinaryTree().maxDepthDFS(root));
+        System.out.println(new MaximumDepthOfBinaryTree().maxDepthBFS(root));
+    }
+
+    private int maxDepthBFS(TreeNode root) {
+        // BFS
+        // BFS
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+
+    private int maxDepthDFS(TreeNode root) {
+        // DFS
+        int max = 0;
+        if (root == null) {
+            return 0;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> value = new Stack<>();
+        stack.push(root);
+        value.push(1);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int temp = value.pop();
+            max = Math.max(temp, max);
+
+            if (node.left != null) {
+                stack.push(node.left);
+                value.push(temp + 1);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+                value.push(temp + 1);
+            }
+        }
+        return max;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
