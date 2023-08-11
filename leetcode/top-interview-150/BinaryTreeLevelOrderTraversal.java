185ab92931f0fdb6fec347a27d0e82edb44fc627
Raj Kundalia <rajakundalia@gmail.com>
2023-08-12 00:13:03 +0530

    Add files via upload

---
+++ leetcode/top-interview-150/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,68 @@
+package leetcode.topinterview150;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values.
+(i.e., from left to right, level by level).
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ */
+public class BinaryTreeLevelOrderTraversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node20 = new TreeNode(20);
+        TreeNode node15 = new TreeNode(15);
+        TreeNode node7 = new TreeNode(7);
+
+        root.left = node9;
+        root.right = node20;
+
+        node20.left = node15;
+        node20.right = node7;
+
+        System.out.println(new BinaryTreeLevelOrderTraversal().levelOrder(root));
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new LinkedList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<>();
+
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                subList.add(queue.poll().val);
+            }
+            result.add(subList);
+        }
+        return result;
+    }
+}
