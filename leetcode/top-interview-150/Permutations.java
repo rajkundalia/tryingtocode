9238ce92f93f0111e69686bb5a776114e7e61caa
Raj Kundalia <rajakundalia@gmail.com>
2023-09-09 11:38:04 +0530

    permutations

---
+++ leetcode/top-interview-150/Permutations.java
@@ -0,0 +1,30 @@
+package leetcode.topinterview150;
+
+import java.util.List;
+
+/*
+Given an array nums of distinct integers, return all the possible permutations.
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+ */
+public class Permutations {
+    public static void main(String[] args) {
+        System.out.println(new Permutations().permute(new int[]{1, 2, 3}));
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+return  null;
+    }
+}
